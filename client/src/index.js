6e6a8b2d20322286de9c2f201b7a763feb618e07
Joël Bosch <yopc@joelbosch.nl>
2022-02-12 18:19:44 +0100

    Standard tic tac toe tutorial

--- client/src/index.js
+++ client/src/index.js
@@ -2,27 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  renderSquare(index) {
+    return (
+      <Square
+        value={ this.props.squares[index] }
+        onClick={() => { this.props.onClick(index); }}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -44,15 +47,96 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xLast: false,
+      stepNumber: 0,
+    }
+  }
+
+  next() { return this.state.xLast ? 'O' : 'X' }
+
+  handleClick(index) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history.at(-1);
+    const squares = current.squares.slice();
+    if (this.winner(squares) || squares[index]) return;
+    squares[index] = this.next();
+    this.setState({
+      history: history.concat([{
+        squares,
+      }]),
+      xLast: !this.state.xLast,
+      stepNumber: history.length,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xLast: (step % 2) !== 0,
+    })
+  }
+
+  winner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (const [a, b, c] of lines) {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = this.winner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        `Go to move #${move}` :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button
+            onClick={() => { this.jumpTo(move); }}
+          >
+            {desc}
+          </button>
+        </li>
+      )
+    });
+
+    const status = (()=> {
+      if (winner) return `Winner: ${winner}`;
+      else return `Next player: ${this.next()}`;
+    })();
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(index) => { this.handleClick(index); }}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
