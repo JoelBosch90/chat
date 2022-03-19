b6e1e6abb4780797754ee27d3916a3ceabe97c5f
Joël Bosch <yopc@joelbosch.nl>
2022-03-19 12:58:43 +0100

    Removed old implementation

--- api/lib/api_web/controllers/hello_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule ApiWeb.HelloController do
-  use ApiWeb, :controller
-
-  def message(conn, %{"data" => data}) do
-    render(conn, "hello.html", data: data)
-  end
-end
--- api/lib/api_web/views/hello_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule ApiWeb.HelloView do
-  use ApiWeb, :view
-end
